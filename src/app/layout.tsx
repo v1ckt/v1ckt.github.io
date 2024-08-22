215e17f41dd8a2671fbabfd3ea8d2aec98a2e836
Vicktor Teixeira <vicktoremannoel2@gmail.com>
2024-08-22 12:02:34 -0300

    updated meta tag description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Vicktor Teixeira - Dev",
-  description: "I'm a software engineer who loves to build things for the web.",
+  description:
+    "I'm a computer science bachelor and software engineer who loves to build things for the web. Here you can find some of my projects and learn more about me.",
   authors: [{ name: "Vicktor Teixeira", url: "github.com/v1ckt" }],
   keywords: ["Vicktor Teixeira", "Developer", "Portfolio", "Vicktor"],
 };
@@ -20,11 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name="google-site-verification" content="loyVs7u4l-uiip0zf_XmC5ObHkJ9_bv8qdWaj0PHfNg" />
-      <link rel="canonical" href="https://v1ckt.github.io" />
-      <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        <meta
+          name="google-site-verification"
+          content="loyVs7u4l-uiip0zf_XmC5ObHkJ9_bv8qdWaj0PHfNg"
+        />
+        <link rel="canonical" href="https://v1ckt.github.io" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
                 (function() {
                   const storedTheme = localStorage.getItem('theme');
                   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -32,8 +36,8 @@ export default function RootLayout({
                   document.documentElement.setAttribute('data-theme', theme);
                 })();
               `,
-            }}
-          />
+          }}
+        />
       </head>
       <body className={inter.className}>
         <Header className="z-[100]" />
