761ef342a2eb9f26ff75724b3b3700f16c58d5e6
Vicktor Teixeira <vicktoremannoel2@gmail.com>
2025-12-23 22:26:16 -0300

    fixed not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,36 +2,39 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 absolute w-full h-full z-[9999] text-center px-6 md:px-24 pt-8 md:pt-24 bg-main-bg">
+    <div
+      className='fixed flex flex-col items-center justify-center gap-4 z-[99999999] text-center bg-main-bg px-16'
+      style={{
+        width: "100vw",
+        height: "100vh",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}>
       <div
-        className="w-full h-full fixed top-0 left-0 z-[-1] flex items-center justify-center"
+        className='w-full h-full fixed top-0 left-0 z-[-1] flex items-center justify-center'
         style={{
-          backgroundImage:
-            "linear-gradient(to top, transparent 1%, var(--main-bg), var(--main-bg), transparent 99%), url('/static/images/bg.svg')",
-          backgroundSize: "59rem",
-          backgroundPosition: "var(--bg-pos)",
           opacity: 0.1,
-        }}
-      >
+        }}>
         <h1
-          className="text-[50vmin] mb-[25vmin] mr-[1rem] text-extrabold"
+          className='mb-[25vmin] mr-[1rem] text-extrabold'
           style={{
             backgroundImage:
               "linear-gradient(to top, var(--main-bg), var(--title))",
             WebkitBackgroundClip: "text",
             backgroundClip: "text",
             color: "transparent",
-            opacity: 0.8,
-          }}
-        >
+            opacity: 0.5,
+            fontSize: "50vmin",
+          }}>
           404
         </h1>
       </div>
-      <h1 className="text-title">Oops!</h1>
-      <h4 className="text-text">
+      <h1 className='text-title'>Oops!</h1>
+      <h4 className='text-text'>
         It seems that the page you&apos;re looking for can&apos;t be found.
-        <br className="hidden md:flex" /> Please, consider going back to{" "}
-        <Link href="/" className="text-[21.6px] md:text-2xl text-accent">
+        <br className='hidden md:flex' /> Please, consider going back to{" "}
+        <Link href='/' className='text-[21.6px] md:text-2xl text-accent'>
           home page
         </Link>
         .
